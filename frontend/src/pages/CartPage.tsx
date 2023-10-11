487297fdaf0b318c167f58eca6c63f1ed9c89e4a
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-10-11 10:26:25 -0300

    remover from cart implemented

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -33,6 +33,10 @@ export default function CartPage() {
     navigate('/signin?redirect=/shipping')
   }
 
+  const removeItemHandler = (item: CartItem) => {
+    dispatch({ type: 'CART_REMOVE_ITEM', payload: item })
+  }
+
   return (
     <div>
       <Helmet>
@@ -81,7 +85,10 @@ export default function CartPage() {
                     </Col>
                     <Col>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant={mode}>
+                      <Button
+                        variant={mode}
+                        onClick={() => removeItemHandler(item)}
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
